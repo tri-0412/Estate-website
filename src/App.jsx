4b2a428b25c3e6f5e9f93923be6bb8fe9a3f4398
tri-0412 <tringuyen8826@gmail.com>
2024-11-26 17:28:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Testimonails from "./components/Testimonails";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="w-full overflow-hidden ">
+      <ToastContainer />
+      <Header />
+      <About />
+      <Projects />
+      <Testimonails />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
